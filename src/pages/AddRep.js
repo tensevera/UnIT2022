974cc9aac8e6d4c1162bc1fa21c025f8950e7ee2
DeepBlue321 <frantisek1nemet@gmail.com>
2022-04-13 15:05:20 +0200

    main things

--- src/pages/AddRep.js
+++ src/pages/AddRep.js
@@ -4,11 +4,17 @@ import axios from "axios";
 import List from '@mui/material/List';
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { SettingsVoiceOutlined } from "@material-ui/icons";
 
 function AddRep() {
  const  sklad = "SKLAD"
   const [list,setList] = useState([]);
-  const [isProd, setIsProd] = useState(false);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const [id, setID] = useState("");
+  const [count, setCount] = useState(0);
+
+  const [isProduct,setIsProduct] = useState(null);
 console.log(sklad);
   function getProdukt(id) {
     axios
@@ -25,44 +31,86 @@ console.log(sklad);
       .then((res) => {
         console.log(res);
         const len = res.data.winstrom["skladova-karta"];
-        setIsProd(len.length > 0);
+        setIsProduct(len.length > 0);
         setName(len[0]?.nazev);
       });
   }
 
-  function addLocal(id,name){
-    const item ={
-      id:id,
-      name:name
-    }
-    setList([...list ,item]);
+  function sentProd(id,count) {
+    axios
+      .post(
+        `https://inventura.flexibee.eu/v2/c/firma2/inventura-polozka`,
+      {
+        "winstrom": {
+          "inventura-polozka": [
+           ...list
+            
+          ]
+        }
+      },
+
+        {
+          auth: {
+            username: "uzivatel2",
+            password: "uzivatel2uzivatel2",
+          }
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      });
   }
 
-  const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [input, setInput] = useState("");
 
+  function addLocal(id,name,count){
+    const item ={
+      name: name,
+      count:count,
+      "inventura": 1,
+      "sklad": 4,
+      "cenik": `ean:${id}`,
+      "mnozMjReal": count
+    }
+    setList(prevState => [...prevState, item]);
+    console.log(list);
+  }
+
+ 
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>go back</Button>
       <TextField
         id="outlined-basic"
-        label="EAN"
-        value={input}
+        label="počet"
+        value={count}
         variant="outlined"
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => setCount(e.target.value)}
       />
-      <Button onClick={(e) => getProdukt(input)}>check</Button>
-      <Button onClick={(e) => addLocal(input)}>přidat</Button>
+      <TextField
+        id="outlined-basic"
+        label="EAN"
+        value={id}
+        variant="outlined"
+        onChange={(e) => setID(e.target.value)}
+      />
+
+      <Button onClick={(e) => getProdukt(id)}>check</Button>
+      <Button onClick={(e) => addLocal(id,name,count)}>přidat</Button>
+
+
 
      
 {list.map((l)=>{
-  <h1>{l.name}</h1>
+  return <h1>{l.name} ,{l.count}</h1>
 })}
       <h1>{name}</h1>
+      <Button onClick={(e) => sentProd(list)}>Poslat</Button>
     </div>
   );
+  
+
+
 }
 
 export default AddRep;
