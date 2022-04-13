e0cf4236ff76c5b592fdf7b26d1b89fc860b3234
Vojtěch Zabořil <zaborvoj@fit.cvut.cz>
2022-04-13 11:58:18 +0200

    add rep header

---
+++ src/components/AddRepHeader.js
@@ -0,0 +1,14 @@
+
+
+function AddRepHeader() {
+    return (
+        <div>
+            <input type="number" />
+            <label> x </label>
+            <input type="text"/>
+            <button>Přidat</button>
+        </div>
+    )
+}
+
+export default AddRepHeader
