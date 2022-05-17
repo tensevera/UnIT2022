735f267c3f6f27ea1876c8a079d61238edf50b9e
tensevera <Jan-Severa@seznam.cz>
2022-05-17 11:25:03 +0200

    Gen: Delete components folder - not used

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
